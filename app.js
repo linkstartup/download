0923ce3a34cbd41f36342abad1188a3177ee27d1
david <apple@AppledeMacBook-Pro-3.local>
2019-05-16 14:22:07 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+var moment=require('moment');
+
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+// const server = 'www.indo123.co/';
+// const server = 'http://localhost/';
+const fs = require("fs");
+
+const fileUpload = require('express-fileupload');
+
+const path = require('path');
+var ObjectId = require('mongodb').ObjectID;
+
+const collection = "todo";
+const collection2 = "user";
+const collection3 = "t";
+const collection4 = "tuanzhang";
+const collection5 = "joinmember";
+const hi = "hi";
+
+const app = express();
+
+
+const testFolder = '../torrent';
+
+
+
+app.use(bodyParser.json({
+    limit: '50mb',
+    extended: true
+}));
+
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000
+
+}));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './seed.html'));
+})
+
+
+
+//读取文件目录
+app.post('/seed', (req, res) => {
+
+    fs.readdir(testFolder, (err, files) => {
+        res.json({
+            files:files,
+        })
+    })
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+// Middleware for handling Error
+// Sends Error Response Back to User
+app.use((err, req, res, next) => {
+    res.status(err.status).json({
+        error: {
+            message: err.message
+        }
+    });
+})
+
+app.listen(8888, () => {
+
+    console.log('connected to database, app listening on port 8888');
+});
